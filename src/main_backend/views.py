51d5a6d2ab7876be78b5d1ce50faf5de6a617fbb
Bombo <bombozaur6@gmail.com>
2023-04-16 19:24:33 +0200

    no body handler

--- src/main_backend/views.py
+++ src/main_backend/views.py
@@ -18,41 +18,41 @@ class Ping(View):
         return view
 
     async def get(self, *args, **kwargs):
-        body_unicode = self.request.body.decode('utf-8')
-        body = json.loads(body_unicode)
-        if 'url' in body:
-            url = body['url']
-            mem = cache.get(url)
-            if mem:
-                return JsonResponse({"response": mem}, status=200)
-            else:
-                try:
-                    re = redis.Redis(host='cache', db=0)
-                    chanels = re.pubsub_channels()
-                    if url in chanels:
-                        conn = re.pubsub(ignore_subscribe_messages=True)
-                        conn.subscribe(url)
-                        conn.listen()
-                        mem = cache.get(url)
-                        return JsonResponse({"response": mem}, status=200)
-                    else:
+        if self.request.body.decode('utf-8'):
+            body_unicode = self.request.body.decode('utf-8')
+            body = json.loads(body_unicode)
+            if 'url' in body:
+                url = body['url']
+                mem = cache.get(url)
+                if mem:
+                    return JsonResponse({"response": mem}, status=200)
+                else:
+                    try:
+                        re = redis.Redis(host='cache', db=0)
+                        chanels = re.pubsub_channels()
+                        if url in chanels:
+                            conn = re.pubsub(ignore_subscribe_messages=True)
+                            conn.subscribe(url)
+                            conn.listen()
+                            mem = cache.get(url)
+                            return JsonResponse({"response": mem}, status=200)
+                        else:
+                            try:
+                                async with httpx.AsyncClient() as client:
+                                    response = await client.get(url)
+                            except httpx.RequestError as erro:
+                                return JsonResponse({"error": f"An error occurred while requesting {erro.request.url!r}."},
+                                                    status=400)
+                            cache.set(url, response.text, os.environ['CACHE_TTL'])
+                            re.publish(url, 'START')
+                            return JsonResponse({"response": response.text}, status=200)
+                    except:
                         try:
                             async with httpx.AsyncClient() as client:
                                 response = await client.get(url)
                         except httpx.RequestError as erro:
-                            return JsonResponse({"error": f"An error occurred while requesting {erro.request.url!r}."},
-                                                status=400)
-                        cache.set(url, response.text, os.environ['CACHE_TTL'])
+                            return JsonResponse({"error": f"An error occurred while requesting {erro.request.url!r}."}, status=400)
+                        cache.set(url, response.text, 20)
                         re.publish(url, 'START')
                         return JsonResponse({"response": response.text}, status=200)
-                except:
-                    try:
-                        async with httpx.AsyncClient() as client:
-                            response = await client.get(url)
-                    except httpx.RequestError as erro:
-                        return JsonResponse({"error": f"An error occurred while requesting {erro.request.url!r}."}, status=400)
-                    cache.set(url, response.text, 20)
-                    re.publish(url, 'START')
-                    return JsonResponse({"response": response.text}, status=200)
-        else:
-            return JsonResponse({"error": "Please give url"}, status=400)
+        return JsonResponse({"error": "Please give url"}, status=400)
